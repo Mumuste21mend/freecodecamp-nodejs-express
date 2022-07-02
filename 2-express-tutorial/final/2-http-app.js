133d0d65ccf58773b74d39ff7766b954ccb2b61d
Must <Mumuste.mend@gmail.com>
2022-07-02 21:18:34 +0100

    basic and complicated express app examples

--- 2-express-tutorial/final/2-http-app.js
+++ 2-express-tutorial/final/2-http-app.js
@@ -2,7 +2,9 @@ const http = require('http')
 const { readFileSync } = require('fs');
 // get all files
 const homePage = readFileSync('../navbar-app/index.html')
-
+const homeStyle = readFileSync('../navbar-app/styles.css')
+const homeImage = readFileSync('../navbar-app/logo.svg')
+const homeLogic = readFileSync('../navbar-app/browser-app.js')
 
 http
     .createServer()
@@ -10,14 +12,25 @@ http
         const url = req.url
         if (url === '/') {
             res.writeHead(200, { 'content-type': 'text/html' })
-            res.write('<h1> Home page </h1>')
+            res.write(homePage)
             return res.end()
 
         } else if (url === '/about') {
             res.writeHead(200, { 'content-type': 'text/html' })
             res.write('<h1> About page </h1>')
             return res.end()
-
+        } else if (url === '/styles.css') {
+            res.writeHead(200, { 'content-type': 'text/css' })
+            res.write(homeStyle)
+            return res.end()
+        } else if (url === '/logo.svg') {
+            res.writeHead(200, { 'content-type': 'image/svg+xml' })
+            res.write(homeImage)
+            return res.end()
+        } else if (url === '/browser-app.js') {
+            res.writeHead(200, { 'content-type': 'text/javascript' })
+            res.write(homeLogic)
+            return res.end()
         } else {
             res.writeHead(404, { 'content-type': 'text/html' })
             res.write('<h1>error </h1>')
---
+++ 2-express-tutorial/final/3-express.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+app.listen(5000, () => {
+    console.log("server is listening on port 5000")
+})
+// app.get
+app.get('/', (req, res) => {
+    res.status(200).send('home page')
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send('about page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1> resource not found </h1>')
+})
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
