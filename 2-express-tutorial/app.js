ac64be918b657b1990566b9d5d9a9c7b0e2b5f1e
Must <Mumuste.mend@gmail.com>
2022-07-02 23:46:26 +0100

    making complex api

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,18 +1,36 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-// setup static and middleware
-app.use(express.static('./public'))
-// alternative approach :
-// adding to static assets 
-// SSR
+const express = require('express');
+const app = express();
+const { products, people } = require('./data')
+
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, 'navbar-app', 'index.html'))
+    // res.status(200).json([{ name: 'mumuste' }, { name: 'mend' }])
+    res.send('<h1>Home Page </h1> <a href="/api/products">api right here </a>')
+})
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map(product => {
+        const { id, name, price } = product
+        return { id, name, price }
+    })
+    res.status(200).json(newProducts)
+})
+app.get('/api/products/:productID', (req, res) => {
+    const { productID } = req.params
+    const singleProduct = products.find(product => product.id === Number(productID))
+    if (!singleProduct)
+        return res.status(404).send("product does not exist ")
+    return res.status(200).json(singleProduct)
 })
 
-app.all('*', (req, res) => {
-    res.status(404).send('resources not found')
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    const { productID, reviewID } = req.params
+    return res.status(200).json({ productID, reviewID })
+})
+
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    return res.send(req.query)//.status(200).json({ productID, reviewID })
 })
 app.listen(5000, () => {
-    console.log("server is listening ")
+    console.log("Server is listening to port 5000")
 })
